42019dfc47d20d46285f3186e6bf5c0672cd33c9
Todd Halkowski <thalkowski@virtusa.com>
2015-06-22 17:21:35 -0500

    initial commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Todd Halkowski
+//  
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var counter = 1
+    
+    var timer = NSTimer()
+    
+    var isAnimating = true
+    
+    @IBOutlet var alienImage: UIImageView!
+
+    @IBAction func updateImage(sender: AnyObject) {
+        
+        if isAnimating == true {
+            
+            timer.invalidate()
+            
+            isAnimating = false
+            
+        } else {
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            
+            isAnimating = true
+            
+        }
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    func doAnimation() {
+        
+        if counter == 5 {
+            
+            counter = 1
+            
+        } else {
+            
+            counter++
+            
+        }
+        
+        alienImage.image = UIImage(named: "frame\(counter).png")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    override func viewDidLayoutSubviews() {
+        
+        alienImage.frame = CGRectMake(100, 20, 0, 0)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        UIView.animateWithDuration(1, animations: { () -> Void in
+            
+            self.alienImage.frame = CGRectMake(100, 20, 100, 200)
+            
+        })
+        
+        
+    }
+    
+*/
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+}
+
